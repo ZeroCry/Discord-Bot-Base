1781b0bbd60905afcb394d04fbd0e424b686b6e0
Passive <passivemodding@gmail.com>
2017-10-11 01:04:45 +1100

    Initial Commit

---
+++ CoreBot/Commands/Example.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace CoreBot.Commands
+{
+    public class Example : ModuleBase
+    {
+        [Command("Example")]
+        public async Task ExampleCommand()
+        {
+            await ReplyAsync("This is an example command");
+        }
+    }
+}
